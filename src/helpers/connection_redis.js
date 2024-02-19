02f3593f1514d6153c632641bff9894eadd3035b
unknown <quangvinhminhthoa@gmail.com>
2024-02-19 14:09:15 +0700

    LoginApi

--- src/helpers/connection_redis.js
+++ src/helpers/connection_redis.js
@@ -2,7 +2,11 @@ const redis = require("redis");
 
 // Create a Redis client
 const client = redis.createClient({
-  url: "redis://127.0.0.1:6380",
+  password: "jrzkUNcRi7vumYFY3zsRZAsLhFsI2E1a", // Thay thế bằng mật khẩu thực tế của bạn
+  socket: {
+    host: "redis-13723.c295.ap-southeast-1-1.ec2.cloud.redislabs.com", // Thay thế bằng host thực tế của bạn
+    port: 13723, // Thay thế bằng port thực tế của bạn
+  },
 });
 client.connect().catch(console.error);
 client
